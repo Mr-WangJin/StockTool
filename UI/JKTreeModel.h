874a2816c2a60417e4269f978f396188ac1fb02a
wangjl-f <WANGJL-F@glodon.com>
2018-03-22 14:34:55 +0800

    开始改造动态加载item child

--- UI/JKTreeModel.h
+++ UI/JKTreeModel.h
@@ -11,26 +11,25 @@ class JKTreeModel: public QAbstractItemModel
 public:
     explicit        JKTreeModel(QObject *parent = 0);
 
-    int             columnCount(const QModelIndex &parent) const;
-    QVariant        data(const QModelIndex &index, int role) const;
-    bool            setData(const QModelIndex &index, const QVariant &value, int role);
-    QModelIndex     index(int row, int column, const QModelIndex &parent) const;
-    QModelIndex     parent(const QModelIndex &child) const;
-    int             rowCount(const QModelIndex &parent) const;
-    Qt::ItemFlags   flags(const QModelIndex &index) const;
-    QVariant        headerData(int section, Qt::Orientation orientation, int role) const;
+    int             columnCount(const QModelIndex &parent) const override;
+    QVariant        data(const QModelIndex &index, int role) const override;
+    bool            setData(const QModelIndex &index, const QVariant &value, int role) override;
+    QModelIndex     index(int row, int column, const QModelIndex &parent) const override;
+    QModelIndex     parent(const QModelIndex &child) const override;
+    int             rowCount(const QModelIndex &parent) const override;
+    Qt::ItemFlags   flags(const QModelIndex &index) const override;
+    QVariant        headerData(int section, Qt::Orientation orientation, int role) const override;
     virtual bool    removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) override;
 
-    void            setRootItem(JKTreeModelItem *rootItem);
-    const JKTreeModelItem *getRootItem() const;
-    JKTreeModelItem   *getRootItem();
-    QModelIndex     indexFromItem(const JKTreeModelItem *item) const;
-    JKTreeModelItem   *itemFromIndex(const QModelIndex &index);
-    const JKTreeModelItem
-                    *itemFromIndex(const QModelIndex &index) const;
+    void					setRootItem(JKTreeModelItem *rootItem);
+    const JKTreeModelItem	*getRootItem() const;
+    JKTreeModelItem			*getRootItem();
+    QModelIndex				indexFromItem(const JKTreeModelItem *item) const;
+    JKTreeModelItem			*itemFromIndex(const QModelIndex &index);
+    const JKTreeModelItem	*itemFromIndex(const QModelIndex &index) const;
 
 
-    virtual void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override;
+    virtual void			sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override;
 
 private:
     QSharedPointer<JKTreeModelItem>   rootItem;
